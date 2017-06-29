ce2ac7ca2376c071c090fb0ceea3802bc46f71f5
Georgi Angelov <georgi.n.angelov@gmail.com>
2017-06-29 22:08:25 +0300

    Initial commit

---
+++ Multidimentional Arrays/Sequence in a Matrix/MatrixSequence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sequence_in_a_Matrix
+{
+    class MatrixSequence
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine().Split();
+            int lines = int.Parse(numbers[0]);
+            int strings = int.Parse(numbers[1]);
+
+            int[,] matrix = new int[lines, strings];
+            for (int row = 0; row < lines; row++)
+            {
+                var values = (Console.ReadLine().Split(' '));
+                for (int column = 0; column < strings; column++)
+                {
+                    matrix[row, column] = int.Parse(values[column]);
+                }
+            }
+
+        }
+    }
+}
