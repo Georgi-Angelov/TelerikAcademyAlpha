f6df60f0a6c94c9a31f3f6d52e1fa86a39c50fce
Georgi Angelov <georgi.n.angelov@gmail.com>
2017-07-03 17:38:59 +0300

    Initial Commit

---
+++ Methods/ReverseNumber/ReverseDecimal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseNumber
+{
+    class ReverseDecimal
+    {
+        static void Main(string[] args)
+        {
+            char[] number = Console.ReadLine().ToCharArray().Reverse().ToArray();
+            Console.WriteLine(number);
+        }
+    }
+}
