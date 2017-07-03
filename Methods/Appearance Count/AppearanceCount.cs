6559da7081db0c1ae02d320d7b944a12388fa00f
Georgi Angelov <georgi.n.angelov@gmail.com>
2017-07-03 17:00:46 +0300

    Initial Commit

---
+++ Methods/Appearance Count/AppearanceCount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Appearance_Count
+{
+    class AppearanceCount
+    {
+        static void Main(string[] args)
+        {
+            int arraySize = int.Parse(Console.ReadLine());
+            var numbers = Console.ReadLine().Split(' ').ToArray();
+            int searchedNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(NumberCount(numbers, searchedNumber));
+        }
+
+        private static int NumberCount(string[] numbers, int searchedNumber)
+        {
+            int count = 0;
+            foreach (var number in numbers)
+            {
+                if (int.Parse(number) == searchedNumber)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
--- Methods/EnglishDigit/LastDigitName.cs
+++ Methods/EnglishDigit/LastDigitName.cs
@@ -28,7 +28,7 @@ namespace EnglishDigit
                 case 5: Console.WriteLine("five"); break;
                 case 6: Console.WriteLine("six"); break;
                 case 7: Console.WriteLine("seven"); break;
-                case 8: Console.WriteLine("eigth"); break;
+                case 8: Console.WriteLine("eight"); break;
                 case 9: Console.WriteLine("nine"); break;
             }
             
