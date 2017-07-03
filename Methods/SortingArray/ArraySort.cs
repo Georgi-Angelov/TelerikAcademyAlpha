c4f4f508f7c03bf5f3d36ce35aba7b58cfe678f4
Georgi Angelov <georgi.n.angelov@gmail.com>
2017-07-03 23:10:16 +0300

    Added Solution

---
+++ Methods/SortingArray/ArraySort.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace SortingArray
+{
+    class ArraySort
+    {
+        static void Main(string[] args)
+        {
+            var arraySize = Console.ReadLine();
+            var array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+             
+            Console.WriteLine(string.Join(" ",PerformArraySort(array)));
+        }
+
+        private static int[] PerformArraySort(int[] array)
+        {
+            Array.Sort(array);
+            return array;
+        }
+    }
+}
