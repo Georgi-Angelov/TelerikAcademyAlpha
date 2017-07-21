666862b0f2afeeaf375630132b1af1bc24d1d499
Georgi Angelov <georgi.n.angelov@gmail.com>
2017-07-21 15:56:34 +0300

    Added School class

---
+++ C# OOP/OOP Principles - Part 1/Problem 1. School classes/School.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1.School_classes
+{
+    class School
+    {
+        private List<Classes> schoolClasses;
+        
+        public School()
+        {
+            this.schoolClasses = new List<Classes>();
+        }
+
+        
+        public void AddClass(Classes addClass)
+        {
+            this.schoolClasses.Add(addClass);
+        }
+    }
+}
