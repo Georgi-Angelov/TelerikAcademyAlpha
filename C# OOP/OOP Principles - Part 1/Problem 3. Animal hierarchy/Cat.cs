c236d852d39c6f66b63b9396645d559bb1fdf304
Georgi Angelov <georgi.n.angelov@gmail.com>
2017-07-25 00:34:33 +0300

    Initial commit

---
+++ C# OOP/OOP Principles - Part 1/Problem 3. Animal hierarchy/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_3.Animal_hierarchy
+{
+    public class Cat : Animal, ISound
+    {
+        public void Sound()
+        {
+            Console.WriteLine("Murrrr!");
+        }
+    }
+}
---
+++ C# OOP/OOP Principles - Part 1/Problem 3. Animal hierarchy/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_3.Animal_hierarchy
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var animals = new List<Animal>();
+
+            animals.Add(new Kitten(2, "Jorkata"));
+            animals.Add(new Kitten(6, "Jorkata"));
+            animals.Add(new Dog(15, "Haralampi", "Male"));
+            animals.Add(new Dog(5, "Haralampi", "Male"));
+            animals.Add(new Tomcat(6, "Magelan"));
+            animals.Add(new Tomcat(2, "Magelan"));
+            animals.Add(new Frog(8, "Ribit", "Female"));
+            animals.Add(new Frog(2, "Ribit", "Female"));
+            animals.Add(new Frog(6, "Ribit", "Female"));
+            animals.Add(new Frog(1, "Ribit", "Female"));
+
+            // TODO: calculate avg. age of each animal
+
+        }
+    }
+}
