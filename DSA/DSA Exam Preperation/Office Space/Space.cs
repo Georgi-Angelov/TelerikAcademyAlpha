8ff30a02e7be2743d1ca212614422b751bea6318
Georgi Angelov <georgi.n.angelov@gmail.com>
2017-08-30 14:22:45 +0300

    Last two problems

--- DSA/DSA Exam Preperation/Office Space/Space.cs
+++ DSA/DSA Exam Preperation/Office Space/Space.cs
@@ -1,16 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Office_Space
+﻿namespace Office
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Solution
     {
-        static void Main(string[] args)
+        public const int MaxTasksCount = 50;
+
+        private static int[] memory;
+        private static bool[] beenThereDoneThat = new bool[MaxTasksCount];
+
+        public static void Main()
         {
-            
+            var n = int.Parse(Console.ReadLine());
+            memory = new int[n];
+            var executionTimes = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            var tasks = new KeyValuePair<int, List<int>>[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                var dependencies = Console.ReadLine()
+                    .Split(' ')
+                    .Select(x => int.Parse(x) - 1)
+                    .ToList();
+
+                tasks[i] = new KeyValuePair<int, List<int>>(executionTimes[i], dependencies);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (memory[i] == 0)
+                {
+                    memory[i] = CalculateMinimumTime(tasks, i);
+                }
+            }
+
+            Console.WriteLine(memory.Max());
+        }
+
+        private static int CalculateMinimumTime(KeyValuePair<int, List<int>>[] tasks, int taskId)
+        {
+            if (memory[taskId] != 0)
+            {
+                return memory[taskId];
+            }
+
+            var executionTime = tasks[taskId].Key;
+            var max = int.MinValue;
+
+            foreach (var nextId in tasks[taskId].Value)
+            {
+                if (nextId != -1)
+                {
+                    if (beenThereDoneThat[nextId])
+                    {
+                        // TODO: Fix this shit!
+                        Console.WriteLine(-1);
+                        Environment.Exit(0);
+                    }
+
+                    beenThereDoneThat[nextId] = true;
+                    var next = CalculateMinimumTime(tasks, nextId);
+
+                    if (next > max)
+                    {
+                        max = next;
+                    }
+                    beenThereDoneThat[nextId] = false;
+                }
+            }
+
+            memory[taskId] = (max > 0 ? max : 0) + executionTime;
+
+            return memory[taskId];
         }
     }
 }
--- DSA/DSA Exam Preperation/Online Market/OnlineMarket.cs
+++ DSA/DSA Exam Preperation/Online Market/OnlineMarket.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
 
 namespace Online_Market
 {
@@ -8,8 +10,7 @@ namespace Online_Market
     {
         static void Main(string[] args)
         {
-            Dictionary<decimal, Product> byPrice = new Dictionary<decimal, Product>();
-            Dictionary<string, ICollection<Product>> byType = new Dictionary<string, ICollection<Product>>();
+            var products = new Products();
             StringBuilder output = new StringBuilder();
             string command;
             while ((command = Console.ReadLine()) != "end")
@@ -18,46 +19,124 @@ namespace Online_Market
                 switch (commandParams[0])
                 {
                     case "add":
-                        Product productToAdd = new Product(commandParams[1], commandParams[2], int.Parse(commandParams[3]));
-                        if (byPrice.ContainsKey(productToAdd.Price))
-                        {
-                            output.AppendLine("Error: Product " + productToAdd.Name + " already exists");
-                            break;
-                        }
-
-                        byPrice.Add(productToAdd.Price, productToAdd);
-
-                        if (!byType.ContainsKey(productToAdd.Type))
-                        {
-                            byType.Add(productToAdd.Type, new List<Product>());
-                        }
-
-                        byType[productToAdd.Type].Add(productToAdd);
-                        
-                        output.AppendLine("Ok: Product " + productToAdd.Name + " added successfully");
-
+                        products.AddProduct(commandParams[1], decimal.Parse(commandParams[2]), commandParams[3]);
                         break;
                     
                     case "filter":
-                        
+                        if (commandParams[2] == "type")
+                        {
+                            products.FilterByType(commandParams[3]);
+                        }
+                        if (commandParams[2] == "price")
+                        {
+                            if (commandParams.Length > 5)
+                            {
+                                products.FilterFromMinToMax(decimal.Parse(commandParams[4]), decimal.Parse(commandParams[6]));
+                            }
+                            else if (commandParams[3] == "from")
+                            {
+                                products.FilterFromMin(decimal.Parse(commandParams[4]));
+                            }
+                            else if (commandParams[3] == "to")
+                            {
+                                products.FilterToMax(decimal.Parse(commandParams[4]));
+                            }
+                        }
                         break;
                 }
             }
+            Console.WriteLine(output.ToString());
         }
-
-        public class Product
+        public class Products
         {
+            private readonly Dictionary<string, SortedSet<Product>> byType = new Dictionary<string, SortedSet<Product>>();
+            private readonly SortedSet<Product> byPrice = new SortedSet<Product>();
+            private readonly HashSet<string> products = new HashSet<string>();
+
+            public void AddProduct(string name, decimal price, string type)
+            {
+                var product = new Product(name, price, type);
+
+                if (products.Contains(name))
+                {
+                    Console.WriteLine("Error: Product {0} already exists", name);
+                }
+                else
+                {
+                    products.Add(name);
+                    byPrice.Add(product);
+
+                    if (!byType.ContainsKey(type))
+                    {
+                        byType.Add(type, new SortedSet<Product>());
+                    }
+                    byType[type].Add(product);
+
+                    Console.WriteLine("Ok: Product {0} added successfully", name);
+                }
+            }
+
+            public void FilterByType(string type)
+            {
+                if (!byType.ContainsKey(type))
+                {
+                    Console.WriteLine("Error: Type {0} does not exists", type);
+                }
+                else
+                {
+                    Console.WriteLine("Ok: {0}", string.Join(", ", byType[type].Take(10)));
+                }
+            }
+
+            public void FilterFromMinToMax(decimal min, decimal max)
+            {
+                Console.WriteLine("Ok: {0}", string.Join(", ", byPrice.Where(x => x.Price >= min && x.Price <= max).Take(10)));
+            }
+
+            public void FilterFromMin(decimal min)
+            {
+                Console.WriteLine("Ok: {0}", string.Join(", ", byPrice.Where(x => x.Price >= min).Take(10)));
+            }
+
+            public void FilterToMax(decimal max)
+            {
+                Console.WriteLine("Ok: {0}", string.Join(", ", byPrice.Where(x => x.Price <= max).Take(10)));
+            }
+        }
+        public class Product : IComparable<Product>
+        {
+
             public string Name { get; set; }
+
+            public decimal Price { get; set; }
 
             public string Type { get; set; }
 
-            public int Price { get; set; }
+           
 
-            public Product(string name, string type, int price)
+            public Product(string name, decimal price,string type )
             {
                 this.Name = name;
-                this.Type = type;
                 this.Price = price;
+                this.Type = type;
+                
+            }
+            public int CompareTo(Product other)
+            {
+                var result = this.Price.CompareTo(other.Price);
+
+
+                if (result == 0)
+                {
+                    result = 1;
+                }
+
+                return result;
+            }
+
+            public override string ToString()
+            {
+                return string.Format("{0}({1:G25})", this.Name, this.Price);
             }
         }
     }
