841a19c99d7c39da1d42e95e8c0ce9a5878fd698
Georgi Angelov <georgi.n.angelov@gmail.com>
2017-09-24 15:44:40 +0300

    Medians init commit

---
+++ DSA/DSA Even More DSA/DSA Exam Day 2 2016 2017/5. Medians/MedianNumberInOrderedBag.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+namespace _5.Medians
+{
+    public class MedianNumberInOrderedBag
+    {
+        public static void Main(string[] args)
+        {
+
+            string input = string.Empty;
+            var list = new OrderedBag<int>();
+
+            while ((input = Console.ReadLine()) != "EXIT")
+            {
+                var splitInput = input.Split().ToArray();
+                if (splitInput[0] == "ADD")
+                {
+                    list.Add(int.Parse(splitInput[1]));
+                }
+
+                else
+                {
+                    double median;
+                    if (list.Count % 2 == 0)
+                    {
+                        median = (double)(list[list.Count / 2] + list[list.Count / 2 - 1]) / 2;
+                    }
+                    else
+                    {
+                        median = (double)list[list.Count / 2];
+                    }
+                    Console.WriteLine(median);
+                }
+            }
+        }
+    }
+}
