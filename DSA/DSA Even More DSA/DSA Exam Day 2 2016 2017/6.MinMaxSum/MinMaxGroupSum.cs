1cfa2d05025f59889d58b0a6083ab99c46920eea
Georgi Angelov <georgi.n.angelov@gmail.com>
2017-09-25 01:53:27 +0300

    init commit - not solved

---
+++ DSA/DSA Even More DSA/DSA Exam Day 2 2016 2017/6.MinMaxSum/MinMaxGroupSum.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace _6.MinMaxSum
+{
+    public class MinMaxGroupSum
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                var mAndG = Console.ReadLine()
+                                   .Split()
+                                   .Select(int.Parse)
+                                   .ToArray();
+                var sequenceCount = mAndG[0];
+                var groupsCount = mAndG[1];
+
+                var numbers = Console.ReadLine()
+                                     .Split()
+                                     .Select(int.Parse)
+                                     .ToArray();
+
+
+
+
+                //Console.WriteLine(MinSum(numbers, n, k));
+            }
+
+
+        }
+        ///Maximal sequence of size k
+        //private static int MinSum(int[] numbers, int n, int k)
+        //{
+        //    if (n < k)
+        //    {
+        //        return -1;
+        //    }
+            
+        //    int res = 0;
+        //    for (int i = 0; i < k; i++)
+        //    {
+        //        res += numbers[i];
+        //    }
+
+        //    int curr_sum = res;
+        //    for (int i = k; i < n; i++)
+        //    {
+        //        curr_sum += numbers[i] - numbers[i - k];
+        //        res = Math.Max(res, curr_sum);
+        //    }
+
+        //    return res;
+        //}
+    }
+}
