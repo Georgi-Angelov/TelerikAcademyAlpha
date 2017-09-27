b0977497371110040b642ddb212d01df78b390e9
Georgi Angelov <georgi.n.angelov@gmail.com>
2017-09-27 15:48:48 +0300

    Init commit

---
+++ JavaScript Fundamentals/06. Loops/04. Lexicographically smallest/4. Lexicographically smallest.js
@@ -0,0 +1,14 @@
+function solve(args) {
+  let first = args[0];
+  let second= args[1];
+
+  if (first>second) {
+      return '>';
+  }
+  else if (first<second) {
+      return '<';
+  }
+  else {
+      return '=';
+  }
+}
